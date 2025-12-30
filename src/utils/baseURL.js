fdbe11a27b6f536f6d1070f6161454398a6b7541
OussamaBrinsi <brinsioussama31@gmail.com>
2025-12-30 13:09:38 +0100

    url update

--- src/utils/baseURL.js
+++ src/utils/baseURL.js
@@ -1,5 +1,5 @@
 const getBaseUrl = () => {
-    return "https://book-store-backend-tau-navy.vercel.app"
-}
+  return "https://book-store-backend-2yj94g5zl-brinsi-oussamas-projects.vercel.app";
+};
 
 export default getBaseUrl;
